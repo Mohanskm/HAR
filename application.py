d3199f122cfd51be8b4cc81e050001c4af9cc435
Mohanskm <mohansk10086488@gmail.com>
2024-03-18 22:59:36 +0530

    Deployment Configuration

---
+++ application.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+import os
+
+application = Flask(__name__)
+
+app = application
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for predicting data
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        # Check if a file is uploaded
+        if 'file' not in request.files:
+            return render_template('home.html', error='No file part')
+
+        file = request.files['file']
+
+        # Check if file is selected
+        if file.filename == '':
+            return render_template('home.html', error='No file selected')
+
+        # Check if file has allowed extension
+        if not file.filename.endswith('.csv'):
+            return render_template('home.html', error='File format not supported. Please upload a CSV file.')
+
+
+        # Process the CSV file
+        custom_data = CustomData(csv_file_path=file)
+        data_frame = custom_data.get_data_as_data_frame()
+
+        # Perform prediction
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(data_frame)
+
+
+        return render_template('home.html', results=results[0])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
